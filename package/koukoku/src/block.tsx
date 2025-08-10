d629186e8e181784e887d8f4a6c7a1f2a966ec22
kyohei <kyohei103@outlook.jp>
2025-08-10 17:13:24 +0900

    package ･ 途中

--- package/koukoku/src/block.tsx
+++ package/koukoku/src/block.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Nakami } from "./nakami"
 
 type Komoku = {
--- playground/tsugi/app/page.tsx
+++ playground/tsugi/app/page.tsx
@@ -1,14 +1,21 @@
-import Koukoku from "./block"
-import { Nakami } from "./nakami"
+import { Metadata } from "next"
 
-export default function Index() {
+import { Koukoku, Nakami } from "koukoku"
+
+export const metadata: Metadata = {
+  title: "playground",
+  description: "koukokuの試すところ"
+}
+
+export default function Page() {
   const nakami: Nakami = {
-    id: "ichiban",
-    hitokoto: "彷徨い出会う心ときめく偶然の発見"
+    id: "tsugi",
+    hitokoto: "なんかいい広告"
   }
   return (
     <main className="antialiased p-5 w-full flex flex-col items-center justify-center">
       <div className="max-w-3xl w-full flex flex-col justify-center items-center">
+        <h1 className="text-2xl">koukokuの試すところ</h1>
         <Koukoku nakami={nakami} className="w-full max-w-[450px] aspect-[3/2] bg-neutral-100"/>
       </div>
     </main>
