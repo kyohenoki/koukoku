580d359ef732f82cc314f55fa7d0a52ec7a87cf5
kyohei <kyohei103@outlook.jp>
2025-08-13 20:20:39 +0900

    jojojo あとでコミットし直すやつ

---
+++ uragawa/src/bunseki/log.ts
@@ -0,0 +1,7 @@
+// 作ったとき
+// 読み込まれたとき
+
+function tsukuru() {
+  // timestamp
+  // sessionid
+}
--- uragawa/src/time.ts
+++
@@ -1,6 +0,0 @@
-function mili() {
-  const now = new Date()
-  return now.getTime()
-}
-
-console.log(mili())
